67ba259097c304e3ff3c4fbcdbff3029026806d5
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2023-01-05 11:48:38 -0500

    add your own lipstick

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const lipstickPrice = document.querySelector('#price');
 const lipstickDescription = document.querySelector('#description');
 const likedButton = document.querySelector('#like-btn');
 
-//function fetchAllLipstick () {
+
 fetch("https://makeup-api.herokuapp.com/api/v1/products.json?product_category=lipstick&product_type=lipstick")
     .then(res => res.json())
     .then(lipsticks =>
@@ -47,4 +47,34 @@ lipstickForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const emailValue = document.getElementById('email')
     alert(`${emailValue.value} has been added to the email list!`);
+})
+
+function uponLoad () {
+    fetch("https://makeup-api.herokuapp.com/api/v1/products.json?product_category=lipstick&product_type=lipstick")
+    .then(res => res.json())
+    .then(lipsticks => {
+        detailImage.src = lipsticks[1].image_link
+        lipstickTitle.textContent = lipsticks[1].name
+        lipstickPrice.textContent = lipsticks[1].price
+        lipstickDescription.textContent = lipsticks[1].description
+    })
+    
+}
+
+uponLoad()
+
+const addLiptick = document.querySelector(".add-lip-form")
+addLiptick.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const addedLipName = document.querySelector("#add-lip-name")
+    const addedLipPrice = document.querySelector("#add-price")
+    const addImageUrl = document.getElementById('add-image-url')
+    const addDescription = document.getElementById('add-description')
+    const lipstickImages = document.createElement("img")
+    lipstickImages.src = addImageUrl.value
+    lipstickList.appendChild(lipstickImages)
+    detailImage.src = addImageUrl.value
+    lipstickTitle.textContent = addedLipName.value
+    lipstickPrice.textContent = addedLipPrice.value
+    lipstickDescription.textContent = addDescription.value
 })
